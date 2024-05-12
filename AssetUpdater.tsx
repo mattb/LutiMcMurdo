dcd013b31f12cfd8aa251a32bd382dc9b7f7e751
Matt Biddulph <mb@hackdiary.com>
2024-05-11 22:36:05 -0700

    Update asset updater to allow download, in the hope of an App Store review success.

--- AssetUpdater.tsx
+++ AssetUpdater.tsx
@@ -1,15 +1,46 @@
 import { useEffect, useState, useContext, createContext } from 'react'
 import * as Progress from 'react-native-progress';
 import DocumentPicker from 'react-native-document-picker'
-import { exists as fileExists, readDir, DocumentDirectoryPath } from '@dr.pogodin/react-native-fs';
+import { exists as fileExists, readDir, DocumentDirectoryPath, downloadFile } from '@dr.pogodin/react-native-fs';
 import { subscribe, unzip } from 'react-native-zip-archive'
 import {
-    //StyleSheet,
+    StyleSheet,
     View,
     Text,
     Settings,
+    Pressable,
     Button,
 } from 'react-native';
+import Spacer from './Spacer.tsx';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    centeredView: {
+      padding: 20,
+    },
+    buttonStyle: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 12,
+      paddingHorizontal: 32,
+      borderRadius: 4,
+      elevation: 3,
+      backgroundColor: 'black',
+    },
+    progressTitle: {
+      fontSize: 18,
+      color: 'black',
+    },
+    buttonTitle: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      color: 'white',
+    },
+});
 
 const AssetContext = createContext(null);
 
@@ -19,39 +50,70 @@ export const useAssetPath = () => {
 
 export const AssetUpdater = ({ children }) => {
     const [progress, setProgress] = useState(0.0);
-    const [isShowingProgress, setIsShowingProgress] = useState(false);
+    const [progressMessage, setProgressMessage] = useState("");
     const [assetPath, setAssetPath] = useState('');
 
     const onUpdate = async (): Promise<string> => {
+        const { uri } = await DocumentPicker.pickSingle({
+            type: "public.zip-archive",
+            mode: "open",
+            presentationStyle: 'fullScreen'
+        });
+        return unzipLuti(uri);
+    }
+
+    const unzipLuti = async (uri) => {
         const subscription = subscribe(function ({
             progress: zipProgress,
             filePath,
         }) {
             setProgress(zipProgress);
         });
+        setProgressMessage("Unpacking LUTI from file");
+        setProgress(0.0);
 
-        const { uri } = await DocumentPicker.pickSingle({
-            type: "public.zip-archive",
-            mode: "open",
-            presentationStyle: 'fullScreen'
-        });
         if (uri !== null) {
             const srcPath = decodeURI(uri.substring(7));
             const targetPath = `${DocumentDirectoryPath}/luti-${Date.now()}`;
-            setIsShowingProgress(true);
             setProgress(0.0);
             const unzipPath = await unzip(srcPath, targetPath);
             if (await fileExists(unzipPath + "/asset-manifest.json")) {
                 Settings.set({ admin_mode: 0 });
                 subscription.remove();
-                setIsShowingProgress(false);
                 return unzipPath;
             }
         }
         subscription.remove();
-        setIsShowingProgress(false);
+        setProgressMessage("");
         throw new Error("Didn't find a LUTI website in zip file");
-    }
+    };
+
+    const downloadLuti = async (): Promise<string> => {
+      const url = 'https://lifeundertheice.s3.amazonaws.com/mini-luti-2024-05-11T21-14.zip';
+      // const url = 'http://localhost:9000/mini-luti-2024-05-11T21-14.zip';
+      const destPath = `${DocumentDirectoryPath}/luti-${Date.now()}`;
+
+      setProgressMessage("Downloading LUTI data");
+      setProgress(0.0);
+      const options = {
+        fromUrl: url,
+        toFile: destPath,
+        background: false,
+        progressDivider: 1,
+        progress: (res) => {
+          const progressPercent = (res.bytesWritten / res.contentLength);
+          console.log(`Progress: ${progressPercent.toFixed(2)}%`);
+          // Update your progress state or UI here
+          // https://lifeundertheice.s3.amazonaws.com/mini-luti-2024-05-11T21-14.zip
+          setProgress(progressPercent);
+        },
+      };
+
+      print("DOWNLOADING", url);
+      return downloadFile(options).promise.then(async res => {
+        return await unzipLuti("file://" + destPath);
+      });
+    };
 
     const getLatestAssetDirectory = async (): Promise<string> => {
         if (Settings.get("admin_mode") !== 0) {
@@ -83,12 +145,32 @@ export const AssetUpdater = ({ children }) => {
     }, []);
 
     if (assetPath === '') {
-        return <View style={{ justifyContent: 'center', flex: 1 }}>
-            <Button onPress={async () => {
-                setAssetPath(await onUpdate())
-            }} title="Update" />
-            {progress > 0.0 ? <Progress.Bar progress={progress} width={200} /> : null}
-        </View>;
+        return <View style={styles.container}>
+            <View style={styles.centeredView}>
+              <Text style={styles.progressTitle}>Life Under The Ice needs a dataset to run.</Text>
+              <Text style={styles.progressTitle}>You can either download a test dataset or insert a USB disk containing a LUTI zip file.</Text>
+              <Spacer size={50} vertical />
+              <Pressable 
+                onPress={async () => {
+                    setAssetPath(await onUpdate())
+                }} 
+                style={styles.buttonStyle}>
+                <Text style={styles.buttonTitle}>Update from zip file</Text>
+              </Pressable>
+              <Spacer size={50} vertical />
+              <Pressable 
+                onPress={async () => {
+                  setAssetPath(await downloadLuti())
+                }}
+                style={styles.buttonStyle}>
+                <Text style={styles.buttonTitle}>Download test version</Text>
+              </Pressable>
+              <Spacer size={50} vertical />
+              {progressMessage !== "" ? <Text style={styles.progressTitle}>{progressMessage}</Text> : null}
+              <Spacer size={10} vertical />
+              {progressMessage !== "" ? <Progress.Bar progress={progress} width={200} /> : null}
+            </View>
+          </View>;
     } else {
         return <AssetContext.Provider value={assetPath}>
             {children}
